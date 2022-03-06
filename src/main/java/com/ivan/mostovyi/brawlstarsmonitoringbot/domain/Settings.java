322ab9ec0d52129fe912b345ef6a5cdfa1dedf6b
Ivan Mostovyi <vanya.mostoviy@gmail.com>
2022-03-06 17:46:29 +0200

    Moved from Redis inmemory database to Postgres

--- src/main/java/com/ivan/mostovyi/brawlstarsmonitoringbot/domain/Settings.java
+++ src/main/java/com/ivan/mostovyi/brawlstarsmonitoringbot/domain/Settings.java
@@ -1,20 +1,27 @@
 package com.ivan.mostovyi.brawlstarsmonitoringbot.domain;
 
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
-import org.springframework.data.annotation.Id;
-import org.springframework.data.redis.core.RedisHash;
-import org.springframework.data.redis.core.index.Indexed;
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 
 @Getter
 @Setter
-@RedisHash
+@Entity
+@NoArgsConstructor
 public class Settings {
 
   @Id
+  @Column(nullable = false, updatable = false, length = 40)
+  @GeneratedValue(strategy = javax.persistence.GenerationType.AUTO, generator = "UUID")
+  @GenericGenerator(name = "UUID", strategy = "org.hibernate.id.UUIDGenerator")
   private String chatId;
 
-  @Indexed
   private String clubTag;
 
   public Settings(String chatId) {
